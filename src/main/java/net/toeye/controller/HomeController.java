a52308a97113a3c37376a90f0edc617d5b2ac7d8
goto9ccc <13142010118@qq.com>
2017-03-29 00:50:21 +0800

    修改属性文件

--- src/main/java/net/toeye/controller/HomeController.java
+++ src/main/java/net/toeye/controller/HomeController.java
@@ -1,10 +1,11 @@
 package net.toeye.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * Created by song on 2017-03-08.
@@ -14,6 +15,7 @@ public class HomeController {
 
     @RequestMapping("/hello")
     public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model){
+
         model.addAttribute("name","song");
         return "hello";
     }
